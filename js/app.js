390c38a89ffb3c3afd1a817e22cc56cda4115fbf
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2021-10-10 17:54:39 +0200

    Integrate file uploading + UI tuning

--- js/app.js
+++ js/app.js
@@ -3,10 +3,24 @@ Vue.use(Buefy)
 var app = new Vue({
   el: '#app',
   data: {
-		file: false
+		file: undefined,
+		waitingState: false
   },
   methods: {
-  	test: function(d){console.log("TEST", d)}
+  	uploadFile: f => {
+  		console.log("TEST", f)
+  		if (f.name.split('.').pop().toLowerCase() == "gexf") {
+  			console.log("Proceed")
+  			app.waitingState = true
+  		} else {
+  			app.file = undefined
+  			alert("/!\\ Only GEXF files are accepted.")
+  		}
+  	},
+  	acceptPact: () => {
+  		console.log("OK")
+  	}
   }
 })
+
 
